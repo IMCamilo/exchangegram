71b5e842158f86144099840b6581a3778db03fbd
Camilo <camilo.jorquera.a@gmail.com>
2019-03-13 00:49:23 -0300

    Initial commit

---
+++ src/main/java/imcamilo/com/github/wlx/WlxChallengeApplication.java
@@ -0,0 +1,13 @@
+package imcamilo.com.github.wlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlxChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlxChallengeApplication.class, args);
+	}
+
+}
